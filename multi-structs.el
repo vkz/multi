b2166873bf0af63b9cbc63172c802d0cea7c7a09
Vlad Kozin <vladilen.kozin@gmail.com>
2019-01-11 10:05:41 +0000

    Idea for a generic mu. getter

--- multi-structs.el
+++ multi-structs.el
@@ -114,7 +114,55 @@
 ;; getter macro for every struct IMO for cases when there're multiple objects of
 ;; the same type or you want short names that don't necessarily convey the type of
 ;; the object.
+;;
+;; Here's we could implement a generic getter `mu.':
+(cl-defgeneric mu--get (obj key))
+;;
+;; default method
+(cl-defmethod mu--get ((obj t) key)
+  (condition-case err
+      (cl-struct-slot-value (type-of obj) (sym key) obj)
+    (cl-struct-unknown-slot nil)))
+;;
+;; method when nil
+(cl-defmethod mu--get ((obj (eql nil)) key)
+  nil)
+;;
+;; method for hash-table
+(cl-defmethod mu--get ((obj hash-table) key)
+  (ht-get obj key))
+;;
+;; Generic `mu.' that should work for type that implements `mu--get':
+(defun mu. (table key &rest keys)
+  (when-let ((table (mu--get table key)))
+    (if keys
+        (apply #'mu. table keys)
+      table)))
+;;
+(example
+ (cl-defstruct foo-struct props)
+ (cl-defmethod mu--get ((obj foo-struct) key)
+   (cl-struct-slot-value 'foo-struct (sym key) obj))
 
+ (mu. (ht (:a 1)) :a)
+ (mu. (ht (:a (ht (:b 1)))) :a :b)
+ (mu. (make-foo-struct :props (ht (:a (ht (:b 1))))) :props :a :b)
+
+ ;; default mu--get should work
+ (cl-defstruct bar-struct props)
+ (mu. (make-bar-struct :props (ht (:a (ht (:b 1))))) :props :a :b)
+
+ ;; nested structs should work
+ (mu. (make-bar-struct :props (make-foo-struct :props 1)) :props :props)
+
+ ;; mix of nested ht and structs should work
+ (let* ((foo (make-foo-struct :props (ht (:b 1))))
+        (bar (make-bar-struct :props (ht (:a foo)))))
+   (list
+    (mu. bar :props :a :props :b)
+    (mu. bar :props :a :props :c)))
+ ;; example
+ )
 
 ;; IDEA if we are going the route of this generic associative mu: getter, maybe we
 ;; should go all the way and define all typical associative functions in a generic
