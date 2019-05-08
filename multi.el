8f69b43dfd470f9c9a41fe01330087a5089d5fa7
Vlad Kozin <vladilen.kozin@gmail.com>
2019-05-08 08:58:28 +0100

    Reprovide multi-* from multi.el

---
+++ multi.el
@@ -0,0 +1,10 @@
+;; -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2018, 2019 by Vlad Kozin
+
+(require 'multi-prelude)
+(require 'multi-patterns)
+(require 'multi-structs)
+(require 'multi-methods)
+
+(provide 'multi)
